81a0f61b77cd6ffa98d31d3954f84905521ef5cc
ParadiseLiang <ParadiseLiang@DESKTOP-0724DLD>
2019-05-19 14:59:32 +0800

    update to weather API

--- springboot-jsp-weather/src/main/resources/static/js/main.js
+++ springboot-jsp-weather/src/main/resources/static/js/main.js
@@ -1,15 +1,22 @@
 //retrieve weather data by Api
 function getWeather(){
+	let API_KEY = 'afde2ea62bdba112dd9ee73f8c3ec9b7';
+	let loc = $('#selectedCity').val() + ',AU';
     $.ajax({
         type:"GET",
-        url:"getWeather?selectedCity="+$('#selectedCity').val(),
-        dataType:"json",
+        url:"http://api.openweathermap.org/data/2.5/weather?q=" + loc,
+        data: {
+        	units: 'metric',
+        	APPID: API_KEY
+        },
         success: function(data){
-           	$("#cityName").html(data.cityName);
-           	$("#updatedTime").html(data.updatedTime);
-           	$("#weather").html(data.weather);
-           	$("#temperature").html(data.temperature);
-           	$("#wind").html(data.wind + 'km/h');
+        	var dateObj = new Date();
+        	var weekdayName = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+           	$("#cityName").html(data.name);
+           	$("#updatedTime").html(weekdayName[dateObj.getDay()] + ' ' + dateObj.getHours() + ':' + dateObj.getMinutes());
+           	$("#weather").html(data.weather[0].main);
+           	$("#temperature").html(Math.round(data.main.temp) + ' °C');
+           	$("#wind").html(Math.round(data.wind.speed * 3.6) + 'km/h');
         },
         error:function(jqXHR){
             console.log("Error: "+jqXHR.status);
